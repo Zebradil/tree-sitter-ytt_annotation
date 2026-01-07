1094eec7710b88346b3cd5f8665f716b712c87f4
German Lashevich <german.lashevich@gmail.com>
2026-01-07 20:28:30 +0100

    remove some comments

--- queries/yaml/injections.scm
+++ queries/yaml/injections.scm
@@ -1,17 +1,11 @@
 ; extends
-; vim: set tabstop=2 shiftwidth=2 expandtab:
-
-; START: ytt
 
 ((comment) @injection.content
   (#lua-match? @injection.content "^#@ ")
-  (#offset! @injection.content 0 3 0 0)  ; remove leading "#@"
+  (#offset! @injection.content 0 3 0 0)
   (#set! injection.language "starlark"))
 
-; ytt overlays
 ((comment) @injection.content
   (#lua-match? @injection.content "^#@%a")
-  (#offset! @injection.content 0 2 0 0)  ; remove leading "#@"
+  (#offset! @injection.content 0 2 0 0)
   (#set! injection.language "ytt_annotation"))
-
-; END: ytt
